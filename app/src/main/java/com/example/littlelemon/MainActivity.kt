1d4021c13d9c682774b3275fecedb0344e486e16
Yassine <yassine.sindel@insoft.ma>
2024-02-12 15:24:41 +0100

    Compose

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -1,29 +1,78 @@
 package com.example.littlelemon
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
-import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.littlelemon.ui.theme.LittleLemonTheme
 
-class MainActivity : AppCompatActivity() {
-
-    lateinit var toplayout : LinearLayout
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Column(modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                Text(
+                    text="Little Lemon", fontSize = 32.sp,
+                    color = Color(0xFFF4CE14)
+                )
 
-        toplayout = findViewById(R.id.toplayout)
+                Text(
+                    text= stringResource(id = R.string.chicago), fontSize = 32.sp,
+                    color = Color(0xFFF4CE14)
+                )
+                Row {
+                    Button(onClick = { /*TODO*/ },
+                        border = BorderStroke(1.dp,color=Color.Red),
+                        shape = RoundedCornerShape(10.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = Color.Gray)
 
-        toplayout.setBackgroundColor(Color.parseColor("#495E57"))
-        var textView = TextView(this)
-        textView.text= resources.getText(R.string.little_lemon)
+                    ) {
+                        Text(text = stringResource(id = R.string.order))
+                    }
+                 }
+            }
 
-        textView.setTextColor(Color.parseColor("#F4CE14"))
 
+        }
+    }
+}
 
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
 
-        toplayout.addView(textView)
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LittleLemonTheme {
+        Greeting("Android")
     }
 }
